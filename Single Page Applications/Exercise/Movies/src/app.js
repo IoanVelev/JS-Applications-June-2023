05064da5f2e053c8429b3eea16e7544598537431
Ioan Velev <ioanvelev46@gmail.com>
2023-07-15 18:25:25 +0300

    add tasks

---
+++ Single Page Applications/Exercise/Movies/src/app.js
@@ -0,0 +1,34 @@
+import { homePage } from "./home.js";
+import { loginPage } from "./login.js";
+import { createPage } from "./create.js";
+import { registerPage } from "./register.js";
+import { updateNav } from "./util.js";
+const routes = {
+    '/': homePage,
+    '/login': loginPage,
+    '/register': registerPage,
+    '/create': createPage,
+    '/logout': logoutPage,
+}
+
+document.querySelector('nav').addEventListener('click', onNavigate);
+document.querySelector('#add-movie-button> a').addEventListener('click', onNavigate);
+function onNavigate(event) {
+    if (event.target.tagName == 'A' && event.target.href) {
+        event.preventDefault();
+        const url = new URL(event.target.href);
+        const view = routes[url.pathname];
+
+        if (typeof view == 'function') {
+            view();
+        }
+    }
+}
+
+function logoutPage() {
+    localStorage.removeItem('user');
+    updateNav();
+}
+
+updateNav();
+homePage();
