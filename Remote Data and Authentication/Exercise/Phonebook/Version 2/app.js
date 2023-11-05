24b0b3e80f39b1a33a9d7d648bd5a9498fffae9a
Ioan Velev <ioanvelev46@gmail.com>
2023-11-05 16:44:06 +0200

    update folder

--- Remote Data and Authentication/Exercise/Phonebook/Version 2/app.js
+++ Remote Data and Authentication/Exercise/Phonebook/Version 2/app.js
@@ -1,55 +1,61 @@
 function attachEvents() {
-    const url = `http://localhost:3030/jsonstore/phonebook`;
-    const ulElement = document.getElementById('phonebook');
-    const loadBtn = document.getElementById('btnLoad');
-    const createBtn = document.getElementById('btnCreate');
+  const url = `http://localhost:3030/jsonstore/phonebook`;
+  const ulElement = document.getElementById("phonebook");
+  const person = document.getElementById("person");
+  const phone = document.getElementById("phone");
 
-    const person = document.getElementById('person');
-    const phone = document.getElementById('phone');
+  document.getElementById("btnLoad").addEventListener("click", responseEntries);
+  document.getElementById("btnCreate").addEventListener("click", postEntries);
 
-    loadBtn.addEventListener('click', onClickLoad);
-    createBtn.addEventListener('click', onClickCreate);
+  function responseEntries() {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => loadEntries(data));
+  }
 
-    async function onClickLoad() {
-        ulElement.innerHTML = '';
-        const res = await fetch(url);
-        const data = await res.json();
-        const dataArr = Object.values(data)
-        dataArr.forEach(element => {
-            const liElement = document.createElement('li');
-            const deleteBtn = document.createElement('button');
-            deleteBtn.setAttribute('id', 'btnDelete');
-            deleteBtn.textContent = 'Delete';
-            liElement.textContent = `${element.person}: ${element.phone}`;
-            liElement.setAttribute('id', element._id);
-            liElement.appendChild(deleteBtn);
-            ulElement.appendChild(liElement);
+  function loadEntries(data) {
+    ulElement.innerHTML = "";
+    Object.values(data).forEach((x) => {
+      const liElement = document.createElement("li");
+      const deleteBtn = document.createElement("button");
+      deleteBtn.textContent = "Delete";
+      liElement.textContent = `${x.person}: ${x.phone}`;
+      liElement.id = x._id;
+      liElement.appendChild(deleteBtn);
+      ulElement.appendChild(liElement);
+      deleteBtn.addEventListener('click', deleteEntry);
+    });
+  }
 
-            deleteBtn.addEventListener('click', onClickDelete);
-        });
+  function postEntries() {
+    if (person.value == '' && phone.value == '') {
+      return;
     }
-    async function onClickCreate() {
-        if (person.value !== '' && phone.value !== '') {
+    const data = {
+      person: person.value,
+      phone: phone.value,
+    };
 
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ person: person.value, phone: phone.value })
-            });
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "aplication/json",
+      },
+      body: JSON.stringify(data),
+    });
 
-            person.value = '';
-            phone.value = '';
-            loadBtn.click()
-        }
-    }
+    person.value = "";
+    phone.value = "";
+    document.getElementById("btnLoad").click();
+  }
 
-    async function onClickDelete(ev) {
-        const removedElement = ev.target.parentNode;
-        const id = ev.target.parentNode.id
-        ulElement.removeChild(removedElement);
-        const response = await fetch(`${url}/${id}`, {
-            method: 'DELETE'
-        })
-    }
+  function deleteEntry(e) {
+    const key = e.target.parentNode.id
+    const url = `http://localhost:3030/jsonstore/phonebook/${key}`;
+    e.target.parentNode.remove();
+    fetch(url, {
+      method: 'DELETE'
+    })
+  }
 }
-attachEvents()
+attachEvents();
